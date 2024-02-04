78d4568efc5b2015e7737a190a9ab486f34677fa
Hakob_Hakobyan3 <hakob.hakobyan.-nd@disney.com>
2024-02-04 20:24:19 +0400

    Initial commit

---
+++ foundations/src/main/java/com/epam/training/backend/spring/foundations/SpringFoundationsApplication.java
@@ -0,0 +1,13 @@
+package com.epam.training.backend.spring.foundations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFoundationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFoundationsApplication.class, args);
+	}
+
+}
---
+++ foundations/src/test/java/com/epam/training/backend/spring/foundations/SpringFoundationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.training.backend.spring.foundations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFoundationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
